5313df519f9241fb286d22b4aef223806828d1fa
JIHYE <jye7921@gmail.com>
2023-06-02 11:10:24 +0900

    HelloTest.java 수정

--- HelloTest.java
+++ HelloTest.java
@@ -2,7 +2,8 @@
 public class HelloTest {
    public static void main(String[] args) {
       System.out.println("Hello Git!!");
-      System.out.println("안녕  Git!!");
+      int sum = a + b;
+     
    }
 }
 
